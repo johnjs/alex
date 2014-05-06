5b00bd9bef8c394e2fc7d9e9ce80fb67abf7a33d
Dominik Michalski <d.m.michalski@gmail.com>
2014-05-06 16:36:37 +0200

    [alex-chrome] Intro of karma.js

--- alex-chrome/Gruntfile.js
+++ alex-chrome/Gruntfile.js
@@ -18,9 +18,34 @@ module.exports = function (grunt) {
         watch: {
             files: ['<%= lint.frontend %>'],
             tasks: 'default'
+        },
+
+        karma: {
+            unit: {
+                options: {
+                    files: [
+                        'app/bower_components/angular/angular.min.js',
+                        'app/bower_components/jquery/dist/jquery.min.js',
+                        'app/scripts/**/*.js',
+                        'test/spec/**/*.js'
+                    ],
+                    exclude: [
+                        'app/scripts/background.js',
+                        'app/scripts/contentscript.js'
+                    ]
+                },
+                frameworks: ['jasmine'],
+                reporters: ['story'],
+                port: 9876,
+                colors: true,
+                autoWatch: false,
+                browsers: ['PhantomJS'],
+                captureTimeout: 60000,
+                singleRun: true
+            }
         }
 
     });
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint','karma']);
 };
--- alex-chrome/app/scripts/contentscript.js
+++ alex-chrome/app/scripts/contentscript.js
@@ -49,5 +49,4 @@ window.addEventListener('load', function () {
         scope.$apply();
     });
 
-
 });
